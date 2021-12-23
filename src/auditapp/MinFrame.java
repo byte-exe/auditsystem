35019d6c27d61ef4488f25c4f24288cc042f8f7a
Vyneee <rivaldo_erdany@yahoo.com>
2021-12-23 12:21:18 +0700

    first commit

---
+++ src/auditapp/MinFrame.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package auditapp;
+
+/**
+ *
+ * @author RoXen
+ */
+public class MinFrame {
+ 
+    public static void main(String[] args) {
+        SpalshScreen screen = new SpalshScreen();
+        NewSignin sign = new NewSignin();
+        screen.setVisible(true);
+        try {
+            for (int row = 0; row <=100; row++) {
+                Thread.sleep(100);
+                screen.loadingnumber.setText(Integer.toString(row)+"%");
+                screen.loadingprogress.setValue(row);
+                if (row == 100) {
+                    
+                    screen.setVisible(false);
+                    sign.setVisible(true);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+}
